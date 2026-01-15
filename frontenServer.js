92c1743ef75eb6f3f53eda685a34e32057441841
Navaneeth <knavaneethkumar8@gmail.com>
2026-01-15 10:40:51 +0530

    make sure to send index at all times

--- frontenServer.js
+++ frontenServer.js
@@ -6,24 +6,9 @@ const PORT = process.env.PORT || 6500;
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname)));
 
-app.get('/', (req, res) => {
-  console.log('root')
-  res.sendFile(path.join(__dirname, "media.html"));
-})
-
-app.get(/\/media(.html)?/, (req, res) => {
-  console.log('media')
-  res.sendFile(path.join(__dirname, "media.html"));
-});
-
-app.get('/video', (req, res) => {
-  console.log('video')
-  res.sendFile(path.join(__dirname, "pages", "video.html"));
-})
-
 app.get(/\/*/, (req, res) => {
   console.log('media file loading');
-  res.sendFile(path.join(__dirname, "media.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 
